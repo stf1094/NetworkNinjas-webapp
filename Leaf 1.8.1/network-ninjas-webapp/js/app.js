77d73985eb9dc294f71a0e0fab5614f5b051d400
stf1094 <s.forsberg@alvariumsystems.com>
2017-06-16 10:30:12 -0400

    June 16, 2017 Update
    1. Mostly tedious, repetitive button color changes.
    2. Changes to app.js for accordion tables
    3. Css changes for accordion tables
    4. funnel.html is a test page
    5. ignore the font stuff for now - it was my attempt to create an icon
    font with our icons but it does not work yet
    6. Added the “Downline” menu item to all pages

--- Leaf 1.8.1/network-ninjas-webapp/js/app.js
+++ Leaf 1.8.1/network-ninjas-webapp/js/app.js
@@ -139,3 +139,20 @@ function init(url) {
   $('[rel="popover"],[data-rel="popover"],[data-toggle="popover"]').popover();
 
 }
+
+ /* ---------- Accordian feature in downline tables ---------- */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
